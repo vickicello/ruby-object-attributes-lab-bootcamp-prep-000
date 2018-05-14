1e9f482035cf10a384b072485467ed6fc23bfaf9
Vicki Hamm <vicki.cello@gmail.com>
2018-05-14 06:59:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,17 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+    
 end
