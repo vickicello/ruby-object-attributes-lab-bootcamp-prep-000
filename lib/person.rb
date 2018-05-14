204d91127e36df8986543e8d1619b8eeef9d4c83
Vicki Hamm <vicki.cello@gmail.com>
2018-05-14 07:05:39 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+end
+
+class Person 
+  def initialize(job)
+    @name = job
+  end
+ 
+  def job
+    @job
+  end
+ 
+  def name=(new_job)
+    @job = new_job
+  end
+ 
+end
